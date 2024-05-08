1d305a1707f6351ddb763354ecc75b4300219a6e
algoshimo <3540383238@qq.com>
2024-05-08 21:02:54 +0800

    update

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -25,15 +25,15 @@ pub struct TaskInfo {
 
 /// task exits and submit an exit code
 pub fn sys_exit(exit_code: i32) -> ! {
-    trace!("[kernel] Application exited with code {}", exit_code);
-    exit_current_and_run_next();
+    trace!("[kernel] Application exited with code {}", exit_code); //输出一条日志消息，说明应用程序以特定的状态码退出
+    exit_current_and_run_next(); //退出当前任务并切换到下一个任务
     panic!("Unreachable in sys_exit!");
 }
 
 /// current task gives up resources for other tasks
-pub fn sys_yield() -> isize {
+pub fn sys_yield() -> isize { //让当前任务放弃CPU资源
     trace!("kernel: sys_yield");
-    suspend_current_and_run_next();
+    suspend_current_and_run_next(); //将当前任务挂起，并切换到下一个任务执行
     0
 }
 
@@ -50,8 +50,25 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
     0
 }
 
+// pub enum TaskStatus {
+//     /// uninitialized
+//     UnInit,
+//     /// ready to run
+//     Ready,
+//     /// running
+//     Running,
+//     /// exited
+//     Exited,
+// }
+
+
 /// YOUR JOB: Finish sys_task_info to pass testcases
-pub fn sys_task_info(_ti: *mut TaskInfo) -> isize {
+pub fn sys_task_info(ti: *mut TaskInfo) -> isize {
     trace!("kernel: sys_task_info");
-    -1
+    unsafe {
+        if let TaskStatus::UnInit = (*ti).status {
+            return -1;
+        }
+        0
+    }
 }
